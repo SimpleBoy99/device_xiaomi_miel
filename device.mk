881c6f9db89294a483aa320a043b17c9058167a0
Fraschze97 <Anprasetya97@gmail.com>
2022-05-07 20:22:50 +0100

    fleur: Add priv-app permissions for IMS
    Change-Id: I856f1640f72a6b074e5390c2f4577efe3beb2d6b

--- device.mk
+++ device.mk
@@ -177,7 +177,10 @@ PRODUCT_BOOT_JARS += \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common
-        
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
