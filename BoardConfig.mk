475aa8180f316532b5977e08334a90d4dc39d462
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-12-10 10:24:53 +0000

    fleur: Enable updateable APEX
    Change-Id: Icd14570f0029853debe1977fc00a084d2db85d7c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,5 +10,8 @@ BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_PREBUILT_ELF_FILES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/fleur/BoardConfigVendor.mk
