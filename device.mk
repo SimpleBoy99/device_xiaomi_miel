f4a5c3b3742afbef7565084dcae90bacaf294734
Alessandro Astone <ales.astone@gmail.com>
2022-12-13 18:54:18 +0000

    fleur: Build fastbootd
    Change-Id: I96de109c9e1d858c5786eeb30574f0f8d66c9de7

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
