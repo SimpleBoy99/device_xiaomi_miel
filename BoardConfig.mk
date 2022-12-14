d52c40104bc44a9a111dcdd27094314be944403f
bengris32 <bengris32@protonmail.ch>
2022-12-14 18:16:12 +0000

    [DNM] fleur: Build broken elf prebuilts
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I21e2e0e529dd6ea31c05f374afe94a4d186e1805

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,8 @@
 DEVICE_PATH := device/xiaomi/fleur
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_PREBUILT_ELF_FILES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Inherit the proprietary files
 include vendor/xiaomi/fleur/BoardConfigVendor.mk
