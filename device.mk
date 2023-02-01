837f32116624c4253febf04be404c96c880aa8e8
bengris32 <bengris32@protonmail.ch>
2023-02-01 20:47:33 +0000

    fleur: Source build mtkpower interfaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I39fab0ac23522b872f2455d2cc6d78e95e87b2fe

--- device.mk
+++ device.mk
@@ -266,7 +266,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-V2-ndk_platform.vendor \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
-    android.hardware.power@1.2.vendor
+    android.hardware.power@1.2.vendor \
+    vendor.mediatek.hardware.mtkpower@1.0.vendor \
+    vendor.mediatek.hardware.mtkpower@1.1.vendor \
+    vendor.mediatek.hardware.mtkpower@1.2.vendor
 
 # Rootdir
 PRODUCT_PACKAGES += \
