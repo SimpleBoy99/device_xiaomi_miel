7450ade93850031cf1055ee32fefa965e76c36b4
bengris32 <bengris32@protonmail.ch>
2023-11-25 23:11:58 +0530

    fleur: Switch to common Vibrator AIDL

--- device.mk
+++ device.mk
@@ -355,6 +355,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-mediatekv2
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.mediatek
+    
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
